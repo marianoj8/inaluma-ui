cde4d68125b25983d9ea556957f1f3c96e923894
Mariano JavaSwing <marianogoncalves8@gmail.com>
2023-12-18 23:27:03 +0100

    funcionario interface model has been created

---
+++ src/app/shared/model/funcionario.ts
@@ -0,0 +1,11 @@
+export interface Funcionario{
+id:number;
+createdAt	:string;
+lastModifiedAt	:string;
+totalModified	:number;
+nome	:string;
+sobrenome	:string
+contacto	:string;
+estado	:boolean;
+genero:string;
+}
