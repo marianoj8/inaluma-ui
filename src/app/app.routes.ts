9e8d3755b290587ca25047c49eec6d33dc3a8993
Mariano JavaSwing <marianogoncalves8@gmail.com>
2023-12-20 00:19:28 +0100

    app.routes.ts file has been modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,9 @@
 import { Routes } from '@angular/router';
-import { FuncionarioComponent } from './modules/funcionario/container/funcionario.component';
-import { FuncionarioListPageComponent } from './modules/funcionario/funcionario-list-page/funcionario-list-page.component';
 
 export const routes: Routes = [
-  { path:'', pathMatch:'full', redirectTo: 'funcionarios'},
+  { path: '', pathMatch: 'full', redirectTo: 'funcionarios' },
   {
     path: 'funcionarios',
-    component: FuncionarioComponent,
-    children:[
-      {
-        path: '',
-        component: FuncionarioListPageComponent
-      }
-    ]
+    loadChildren: (() => import('./modules/funcionario/modules/funcionario-routing.module').then((f) => f.FuncionarioRoutingModule)),
   }
 ];
