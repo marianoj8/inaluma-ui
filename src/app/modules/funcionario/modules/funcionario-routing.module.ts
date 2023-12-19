d7cf1d657ebe4b291609d9f4a7cd551d8df7d60a
Mariano JavaSwing <marianogoncalves8@gmail.com>
2023-12-20 00:21:07 +0100

    funcionario-routing.module.ts file has been created

---
+++ src/app/modules/funcionario/modules/funcionario-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FuncionarioComponent } from "../container/funcionario.component";
+import { FuncionarioListPageComponent } from "../funcionario-list-page/funcionario-list-page.component";
+import { FuncionarioFormPageComponent } from "../funcionario-form-page/funcionario-form-page.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FuncionarioComponent,
+    children: [
+      {
+        path: '',
+        component: FuncionarioListPageComponent,
+      },
+      {
+        path: 'add',
+        component: FuncionarioFormPageComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: FuncionarioFormPageComponent,
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FuncionarioRoutingModule { }
