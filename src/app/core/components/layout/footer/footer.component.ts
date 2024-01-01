aea990584bc5ca6ad047e27b76230ad2631271f2
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-01 22:51:25 +0100

    footer component stabilized

--- src/app/core/components/layout/footer/footer.component.ts
+++ src/app/core/components/layout/footer/footer.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { APP_ROUTES } from 'src/app/core/config/routes';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
+  readonly routes = APP_ROUTES;
+  readonly ano: number;
 
-  constructor() { }
+  constructor() {
+    this.ano = new Date(Date.now()).getFullYear();
+  }
 
   ngOnInit(): void {
   }
