a0ddaff5367f3b7cbe3a1d4490fccbfa5aafae5e
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-01 20:26:06 +0100

    header component stabilized

--- src/app/core/components/layout/header/header.component.ts
+++ src/app/core/components/layout/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { APP_ROUTES } from 'src/app/core/config/routes';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  readonly routes = APP_ROUTES;
 
   constructor() { }
 
