78f55e095a6f20bc8f7350f5aeb973538ecf4885
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-04 01:28:04 +0100

    bugfix: Theme service loaded

--- src/app/core/app-core.module.ts
+++ src/app/core/app-core.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { LayoutModule } from "./components/layout/layout.module";
+import { ThemeService } from "./services/theme.service";
 
 @NgModule({
   exports: [
     LayoutModule
+  ],
+  providers: [
+    ThemeService
   ]
 }) export class CoreModule {}
