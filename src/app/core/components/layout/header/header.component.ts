59cf0a5a414b235b119266e9fd1c76461f7c2822
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-05 00:26:49 +0100

    light theme partially implemented

--- src/app/core/components/layout/header/header.component.ts
+++ src/app/core/components/layout/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ThemeService } from 'src/app/core/services/theme.service';
 import { APP_ROUTES } from 'src/app/shared/config';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  /* DEPENDENCIES */
+  private readonly _themeService = inject(ThemeService);
+
+  /* MEMBERS */
   readonly routes = APP_ROUTES;
+  public isDarkThemeMode: boolean;
 
-  constructor() { }
+  constructor() { this.isDarkThemeMode = this._themeService.isDarkMode(); }
 
-  ngOnInit(): void {
+  public toggleDarkMode() {
+    this._themeService.toggleDarkMode().then(res => { this.isDarkThemeMode = res });
   }
-
 }
