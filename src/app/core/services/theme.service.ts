5ea65b557cf04146348bedfe40ff0c808fe691d6
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-04 23:07:15 +0100

    theme service now includes light theme mode

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -12,24 +12,58 @@ export class ThemeService {
   /* MEMBERS */
   private _defaultThemeMode = THEME_CLASSES.dark;
 
-  public init() { this._setTheme(this._defaultThemeMode) }
+  public init() {
+    // If a theme configuration already exists then we should remove it before proceeding
+    if(this._currentThemeModeLS) this._unsetTheme(this._currentThemeModeLS);
 
-  private _setTheme(theme: THEME_CLASSES) {
-    this._renderer.addClass(document.body, theme.toString());
-    this._overlayContainer.getContainerElement().classList.add(theme.toString());
-    localStorage.setItem(LOCAL_STORAGE.themeKey, theme.toString());
+    // install default theme config
+    this._setTheme(this._defaultThemeMode);
   }
 
-  private _deactivateTheme(theme: THEME_CLASSES) {
-    this._renderer.removeClass(document.body, theme.toString());
-    this._overlayContainer.getContainerElement().classList.remove(theme.toString());
+  private _setTheme(theme: THEME_CLASSES): boolean {
+    try {
+      this._manageThemeMode(theme, true);
+      localStorage.setItem(LOCAL_STORAGE.themeKey, theme.toString()); // register the theme mode in the local storage
+
+      return true;
+    } catch (err) {
+      alert('It seems you have disabled storage for our site, in order to proceed, you must activate it!');
+      console.log(err.message);
+
+      return false
+    }
   }
 
-  public toggleDarkMode() {
-
+  private _unsetTheme(theme: THEME_CLASSES) {
+    this._manageThemeMode(theme, false);
+    localStorage.removeItem(LOCAL_STORAGE.themeKey); // unregister the theme mode in the local storage
   }
 
-  public isDarkMode() {
-    return localStorage.getItem(LOCAL_STORAGE.themeKey)
+  private _manageThemeMode(theme: THEME_CLASSES, install: boolean) {
+    if(install) {
+      this._renderer.addClass(document.body, theme.toString()); // install the theme config on all descendants of MatSidenavContainer
+      this._overlayContainer.getContainerElement().classList.add(theme.toString()); // install the theme config on all descendants of MatOverlay
+    } else {
+      this._renderer.removeClass(document.body, theme.toString());
+      this._overlayContainer.getContainerElement().classList.remove(theme.toString());
+    }
+  }
+
+  public toggleDarkMode(): Promise<boolean> {
+    const oldThemeMode = this._currentThemeModeLS;
+    const newThemeMode = this.isDarkMode() ? THEME_CLASSES.light : THEME_CLASSES.dark;
+
+    this._unsetTheme(oldThemeMode);
+    const res = this._setTheme(newThemeMode);
+
+    return res ? Promise.resolve(this.isDarkMode()) : Promise.reject(null);
+  }
+
+  private get _currentThemeModeLS(): THEME_CLASSES {
+    return localStorage.getItem(LOCAL_STORAGE.themeKey) as THEME_CLASSES;
+  }
+
+  public isDarkMode(): boolean {
+    return THEME_CLASSES.dark === this._currentThemeModeLS
   }
 }
