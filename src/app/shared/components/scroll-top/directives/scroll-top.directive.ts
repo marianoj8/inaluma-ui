af2593017e5d0bd709a1a5703db2eb8ff6c90f63
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-04 01:26:45 +0100

    scroll to top function fully implemented

--- src/app/shared/components/scroll-top/directives/scroll-top.directive.ts
+++ src/app/shared/components/scroll-top/directives/scroll-top.directive.ts
@@ -3,18 +3,25 @@ import {
   ComponentRef,
   Directive,
   ElementRef,
+  HostListener,
   Input,
   OnInit,
   Renderer2,
-  ViewContainerRef
+  ViewContainerRef,
+  inject
 } from '@angular/core';
 import { MatSidenavContainer } from '@angular/material/sidenav';
 import { LayoutService } from 'src/app/core/components/layout/layout.service';
 import { SCRTOP_COLOR, SCRTOP_POSITION } from 'src/app/shared/config/consts/types';
 import { ConfigOptions, ScrollTopComponent } from '../scroll-top.component';
+import { throttleTime, map, pairwise, distinctUntilChanged, share, filter, fromEvent } from 'rxjs';
 
 @Directive({ selector: '[appScrollTop]' })
 export class ScrollTopDirective implements OnInit, AfterViewInit {
+  /* DEPENDENCIES */
+  private readonly _viewContainerRef = inject(ViewContainerRef);
+  private readonly _renderer = inject(Renderer2);
+  private readonly _layoutService = inject(LayoutService);
   /** The scroll to top trigger */
   private _trigger: ComponentRef<ScrollTopComponent>;
   private _opts: ConfigOptions;
@@ -35,14 +42,8 @@ export class ScrollTopDirective implements OnInit, AfterViewInit {
     };
   };
 
-  constructor(
-    private viewContainerRef: ViewContainerRef,
-    private _renderer: Renderer2,
-    private _layoutService: LayoutService
-    ) { }
-
   ngOnInit(): void {
-    this._trigger = this.viewContainerRef.createComponent<ScrollTopComponent>(ScrollTopComponent);
+    this._trigger = this._viewContainerRef.createComponent(ScrollTopComponent);
     let pai = this._trigger.location.nativeElement;
     let container = this._trigger.instance.container.nativeElement;
 
@@ -65,21 +66,35 @@ export class ScrollTopDirective implements OnInit, AfterViewInit {
     this._doSubscriptions(elem, container);
   }
 
-  private _doSubscriptions(elem, container) {
-    if(elem instanceof MatSidenavContainer) // scroll listener for MatSidenavContainer
-      (elem as MatSidenavContainer).scrollable.elementScrolled().subscribe((evt: Event) => {
-        this._scrollListener(elem.scrollable.getElementRef(), container, elem);
-      });
-    else // scroll listener for any other element
+  private _doSubscriptions(elem: MatSidenavContainer | HTMLElement, container) {
+    if(elem instanceof MatSidenavContainer) { // scroll listener for MatSidenavContainer
+      const scroll$ = elem.scrollable.elementScrolled().pipe(
+        throttleTime(10), // only emit every 10 ms
+        map((evt) => evt.target), // get vertical scroll position
+        pairwise(), // look at this and the last emitted element
+        map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)), // compare this and the last element to figure out scrolling direction
+        distinctUntilChanged(), // only emit when scrolling direction changed
+        share() // share a single subscription to the underlying sequence in case of multiple subscribers
+      )
+      const goingUp$ = scroll$.pipe(filter(direction => direction === Direction.Up));
+      const goingDown$ = scroll$.pipe(filter(direction => direction === Direction.Down));
+
+      goingUp$.subscribe(() => console.log('scrolling up'));
+      goingDown$.subscribe(() => console.log('scrolling down'));
+
+    } else // scroll listener for any other element
       this._renderer.listen(elem, 'scroll', (evt: Event) => {
         this._scrollListener(elem, container, elem);
       });
 
     this._renderer.listen(this._trigger.instance.trigger.nativeElement, 'click', () => {
-      this._scrollBody(elem, 0);
+      this._scrollContainer(elem, 0);
     });
 
-    this._layoutService.onBodyScrollRequested$.subscribe((scroll) => { this._scrollBody(elem, scroll) });
+    /* this._layoutService.onBodyScrollRequested$.subscribe((scroll) => {this._scrollContainer(elem, scroll) });
+    this._layoutService.onBodyScrollRequested$.subscribe((s) => {
+      console.log('a scroll was requested so I came =)');
+    }); */
   }
 
   /**
@@ -111,8 +126,10 @@ export class ScrollTopDirective implements OnInit, AfterViewInit {
    * @param container the component to fade
    * @param options the options object
    */
-  private _manageVisibility(elem: ElementRef<any>, container: ElementRef<any>, options: ConfigOptions) {
-    this._applyCSSClass(container.nativeElement, 'hide', !(elem.nativeElement.scrollTop < options.scrollHeight));
+  private _manageVisibility(elem: any, container: ElementRef<any>, options: ConfigOptions) {
+    let cont = container.nativeElement ?? container;
+    let targ = elem.nativeElement ?? elem
+    this._applyCSSClass(cont, 'hide', !(targ.scrollTop < options.scrollHeight));
   }
 
   private _scrollListener(elem, container, source) {
@@ -120,10 +137,23 @@ export class ScrollTopDirective implements OnInit, AfterViewInit {
     this._layoutService.bodyScrolled(source);
   }
 
-  private _scrollBody(elem: any, scroll: number) {
-    (elem instanceof MatSidenavContainer)
-      ? (elem as MatSidenavContainer).scrollable.scrollTo({ behavior: 'smooth', top: scroll })
-      : elem.nativeElement.scrollTop = scroll;
+  private _scrollContainer(elem: MatSidenavContainer | HTMLElement, scroll: number) {
+    const scrollOptions: ScrollToOptions = { behavior: 'smooth', top: scroll };
+    if(elem instanceof MatSidenavContainer) elem.scrollable.scrollTo(scrollOptions)
+    else elem.scrollTo(scrollOptions);
     this._layoutService.bodyScrolled(elem);
   }
+
+  @HostListener('document:scroll', ['$event.target'])
+  public onDocumentScroll(target) {
+    this._scrollListener(target.scrollingElement, this._trigger.instance.container, target);
+    this._renderer.listen(this._trigger.instance.trigger.nativeElement, 'click', (e) => {
+      this._scrollContainer(target.scrollingElement, 0);
+    });
+  }
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
 }
