5d33a82898af5d45eaa36af802561d59f800cfb3
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-08 01:06:23 +0100

    startup script and compose.yaml files updated

---
+++ ci_interactive.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+re=[^br]
+
+echo "What would you like to do? (b -> build, r -> run, q -> quit): "
+read -r choice
+
+if [[ $choice =~ $re ]]; then
+  if [ $choice = q ]; then
+    echo see ya
+  else
+    echo "I understand! Technology is not easy..."
+  fi
+  exit 0
+else
+  set -e
+  if [ $choice = b ]; then
+    echo "stopping and flushing containers"
+    docker-compose down -v
+
+    echo 'removing api image'
+    docker rmi -f marianojs8/inaluma-api:arm64-v8-1.0-beta
+
+    echo "pulling images and raising containers"
+    docker-compose up -d --build --force-recreate
+  else
+    docker-compose up -d
+  fi
+fi
