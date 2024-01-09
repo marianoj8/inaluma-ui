a1fd08ae641553bdf918196fa4273b014d477473
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-09 12:19:31 +0100

    fixed api endpoint paths

--- src/app/shared/services/items.service.ts
+++ src/app/shared/services/items.service.ts
@@ -9,22 +9,48 @@ export class ItemsService {
   /* DEPENDENCIES */
   private readonly _http = inject(HttpClient);
 
-  public fetch(isProduto: boolean) { return this._fetchItem(this._getPath(isProduto)); }
-  public getItemByID(id: number, isProduto: boolean) { return this._getItemByID(id, this._getPath(isProduto)); }
-  public registerItem(item: ItemDTO, isProduto: boolean) { return this._saveItem(item, this._getPath(isProduto)); }
-  public updateItem(item: ItemDTO, isProduto: boolean) { return this._updateItem(item, this._getPath(isProduto)); }
-  public deleteItemByID(idItem: number, isProduto: boolean) { return this._deleteItemByID(idItem, this._getPath(isProduto)); }
+  public fetch(isProduto: boolean) { return this._fetchItem(this._getPath(isProduto, Operation.fetch)); }
+  public getItemByID(id: number, isProduto: boolean) { return this._getItemByID(id, this._getPath(isProduto, Operation.get)); }
+  public registerItem(item: ItemDTO, isProduto: boolean) { return this._saveItem(item, this._getPath(isProduto, Operation.save)); }
+  public updateItem(item: ItemDTO, isProduto: boolean) { return this._updateItem(item, this._getPath(isProduto, Operation.update)); }
+  public deleteItemByID(idItem: number, isProduto: boolean) { return this._deleteItemByID(idItem, this._getPath(isProduto, Operation.delete)); }
 
   // Métodos internos
-  private _fetchItem(path: ItemPath) { return this._http.get<ItemDTO[]>(environment.API+path); }
-  private _getItemByID(itemID: number, path: ItemPath) { return this._http.get<ItemDTO>(environment.API+path+itemID); }
-  private _saveItem(item: ItemDTO, path: ItemPath) { return this._http.post<ItemDTO>(environment.API+path, item); }
-  private _updateItem(item: ItemDTO, path: ItemPath) { return this._http.put<ItemDTO>(environment.API+path, item); }
-  private _deleteItemByID(itemId: number, path:ItemPath) { return this._http.delete<void>(environment.API+path+itemId)}
+  private _fetchItem(path: string) { return this._http.get<ItemDTO[]>(path); }
+  private _getItemByID(itemID: number, path: string) { return this._http.get<ItemDTO>(path+itemID); }
+  private _saveItem(item: ItemDTO, path: string) { return this._http.post<ItemDTO>(path, item); }
+  private _updateItem(item: ItemDTO, path: string) { return this._http.put<ItemDTO>(path, item); }
+  private _deleteItemByID(itemId: number, path:string) { return this._http.delete<void>(path+itemId)}
 
-  private _getPath(isProduto: boolean): ItemPath {
-    return isProduto ? API_PRODUTOS_ROUTES.fetch : API_SERVICES_ROUTES.fetch;
+  private _getPath(isProduto: boolean, operation: Operation): string {
+    let path = environment.API;
+
+    switch (operation) {
+      case Operation.fetch:
+        path += isProduto ? API_PRODUTOS_ROUTES.fetch : API_SERVICES_ROUTES.fetch;
+        break;
+      case Operation.get:
+        path += isProduto ? API_PRODUTOS_ROUTES.getByID : API_SERVICES_ROUTES.getByID;
+        break;
+      case Operation.save:
+        path += isProduto ? API_PRODUTOS_ROUTES.create : API_SERVICES_ROUTES.create;
+        break;
+      case Operation.delete:
+        path += isProduto ? API_PRODUTOS_ROUTES.deleteByID : API_SERVICES_ROUTES.deleteByID
+        break;
+      case Operation.update:
+        path += isProduto ? API_PRODUTOS_ROUTES.update : API_SERVICES_ROUTES.update;
+        break;
+    }
+
+    return path;
   }
 }
 
-type ItemPath = API_PRODUTOS_ROUTES | API_SERVICES_ROUTES;
+enum Operation {
+  fetch,
+  get,
+  save,
+  delete,
+  update
+}
