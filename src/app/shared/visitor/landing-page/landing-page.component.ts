20c00305da825bdff1481efad0e2701a063409bf
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-09 14:30:11 +0100

    landing page stable

--- src/app/shared/visitor/landing-page/landing-page.component.ts
+++ src/app/shared/visitor/landing-page/landing-page.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { APP_ROUTES } from '../../config';
 import { ItemDTO } from 'src/app/core/model/dto/ItemDTO';
+import { ItemsService } from '../../services/items.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html'
 })
 export class LandingPageComponent implements OnInit {
+  /* DEPENDENCIES */
+  private readonly _itemsService = inject(ItemsService);
+
+  /* MEMBERS */
   readonly pathFotoCapa = '../../../../assets/images/capa.png.jpg';
-  public items: ItemDTO[];
+  public produtos: ItemDTO[];
+  public servicos: ItemDTO[];
   public readonly routes = APP_ROUTES;
 
+  constructor() {
+    this._itemsService.fetch(true).pipe(take(4)).subscribe(items => { this.produtos = items });
+    this._itemsService.fetch(false).pipe(take(4)).subscribe(items => { this.servicos = items });
+  }
+
   ngOnInit(): void {
+    this._itemsService.fetch(true)
   }
 }
