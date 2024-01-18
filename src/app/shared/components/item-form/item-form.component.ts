4ed0d4973c838b595ddef8cbae2c800e55fbef72
Piedoso Pedro <piedosopedro@gmail.com>
2024-01-18 10:11:21 +0100

    implemented update item feature

--- src/app/shared/components/item-form/item-form.component.ts
+++ src/app/shared/components/item-form/item-form.component.ts
@@ -6,7 +6,7 @@ import { ItemsService } from "../../services/items.service";
 import { FilesService } from "../../services/files.service";
 import { Observable, forkJoin, from, fromEvent, map, of, startWith, switchAll, switchMap } from "rxjs";
 import { ImageFile } from "src/app/core/model/dto/ImageFile";
-import { IDialogsResponses, TiposProdutos } from "src/app/core/model";
+import { IDialogsConfig, IDialogsResponses, TiposProdutos } from "src/app/core/model";
 import { MatButtonToggleChange } from "@angular/material/button-toggle";
 import { MatDialog } from "@angular/material/dialog";
 import { ConfirmDialogComponent } from "../dialogs/confirm-dialog/confirm-dialog.component";
@@ -162,52 +162,57 @@ export class ItemFormComponent implements OnInit {
 
     Object.assign(this._item.item, this.itemForm.value);
 
-    if(this._item.id) {
-      this._itemsService.updateItem(this._item.item, this.isProduto).pipe(
-        map((itm) => {
-          this._item = itm;
+    if(this._item.id) this._update();
+    else this._save();
+  }
 
-          this._filesService.uploadImage(new ImageFile(this._file), this._item.id, this.isProduto)
-        }),
-      ).subscribe(() => {
-        const values: Values = {
-          nome: this._item.item.nome,
-          preco: this._item.item.preco,
-          descricao: this._item.item.descricao,
-        }
+  private _update(): void {
+    this._itemsService.updateItem(this._item.item, this.isProduto).pipe(
+      map((itm) => {
+        this._item = itm;
 
-        if(this.isProduto) {
-          values.stock = this._item.item.stock;
-          values.tipo = this._item.item.tipo;
-          values.code = this._item.item.code;
-        } else {
-          values.duracao = this._item.item.duracao;
-          values.units = this._item.item.units;
-        }
+        this._filesService.uploadImage(new ImageFile(this._file), this._item.id, this.isProduto)
+      }),
+    ).subscribe(() => {
+      const values: Values = {
+        nome: this._item.item.nome,
+        preco: this._item.item.preco,
+        descricao: this._item.item.descricao,
+      }
 
-        this._patchData(values, this.itemForm);
-        this.showProgressBar = false;
-      });
-    } else {
-      this._itemsService.registerItem(this._item.item, this._item.isProduto).pipe(
-        switchMap(item => {
-          let resp: {file: Observable<File>, id: Observable<number>};
+      if(this.isProduto) {
+        values.stock = this._item.item.stock;
+        values.tipo = this._item.item.tipo;
+        values.code = this._item.item.code;
+      } else {
+        values.duracao = this._item.item.duracao;
+        values.units = this._item.item.units;
+      }
 
-          if(this.hasFile) resp = {file: of(this._file), id: of(item.id)};
-          else resp = {file: this._itemsService.getNoImage(), id: of(item.id)}
+      this._patchData(values, this.itemForm);
+      this.showProgressBar = false;
+    });
+  }
 
-          return forkJoin(resp);
-        }),
-        switchMap(data => this._filesService.uploadImage(new ImageFile(data.file), data.id, this.isProduto))
-      ).subscribe(() => {
-        this.showProgressBar = false;
-        this.limparCampos();
-      })
-    }
+  private _save(): void {
+    this._itemsService.registerItem(this._item.item, this._item.isProduto).pipe(
+      switchMap(item => {
+        let resp: {file: Observable<File>, id: Observable<number>};
+
+        if(this.hasFile) resp = {file: of(this._file), id: of(item.id)};
+        else resp = {file: this._itemsService.getNoImage(), id: of(item.id)}
+
+        return forkJoin(resp);
+      }),
+      switchMap(data => this._filesService.uploadImage(new ImageFile(data.file), data.id, this.isProduto))
+    ).subscribe(() => {
+      this.showProgressBar = false;
+      this.limparCampos();
+    })
   }
 
   unidadeChanged(evt: MatButtonToggleChange) { this.itemForm.get('units').setValue(evt.value) }
-  canSave(): boolean { return this.itemForm.valid }
+  canSave(): boolean { return this.itemForm.valid && this.itemForm.dirty }
   get isProduto(): boolean { return this._item.isProduto }
 
   public get duracao(): string {
@@ -248,7 +253,28 @@ export class ItemFormComponent implements OnInit {
 
   public limparCampos(): void {
     this.imagePath = this._chooseImageIcon;
-    this.itemForm.reset({emitEvent: false});
+    this._item = new Item(new ItemDTO(), this.isProduto);
+    this._file = null;
+    this.itemForm.reset(undefined, {emitEvent: false});
+  }
+
+  public cancelar(): void {
+    if(this.itemForm.dirty) {
+      this._diagService.open(
+        ConfirmDialogComponent,
+        {
+          disableClose: true,
+          data: {
+            controls: DIALOG_CONTROLS.yes_no,
+            isNegativeWarn: false,
+            message: 'Se cancelar agora, qualquer alteração que tenha feito será ignorada! Deseja prosseguir?',
+            title: 'Confirmar Cancelar'
+          }
+        }
+      ).afterClosed().subscribe((resp: IDialogsResponses) => {
+        if(resp.response === DIALOG_RESPONSES.yes) history.back();
+      });
+    } else history.back();
   }
 
   private _patchData(data: Values, form: FormGroup): void { this.itemForm.patchValue({...data}) }
