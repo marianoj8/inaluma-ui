fddb2355bbb2b6f21dca6d1e3880c6f161586d01
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-10 00:20:56 +0100

    set material symbols as default font set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Inaluma MakeUp';
+  private readonly _matIconRegistry = inject(MatIconRegistry);
+  public title = 'Inaluma MakeUp';
 
   constructor() {
     document.head.getElementsByTagName('title')[0].innerText = this.title;
+    this._matIconRegistry.setDefaultFontSetClass('material-symbols-outlined');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/app-core.module';
 import { SharedModule } from './shared/shared.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { CustomErrorStateMatcher } from './shared/config/validators/form';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,8 +23,7 @@ registerLocaleData(localePt, 'pt');
     HttpClientModule,
     BrowserAnimationsModule,
     CoreModule,
-    SharedModule,
-    FontAwesomeModule,
+    SharedModule
   ],
   providers: [
     { provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher },
